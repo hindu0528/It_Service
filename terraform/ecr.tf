bd50da0baf0ef7862b3ab938f949bed0577e9358
hindu0528 <hindu0528@gmail.com>
2026-08-07 15:02:50 +0530

    ecr

---
+++ terraform/ecr.tf
@@ -0,0 +1,23 @@
+# Define all microservice ECR repositories in a clean loop
+resource "aws_ecr_repository" "repos" {
+  for_each             = toset([
+    "discovery-server",
+    "config-server",
+    "api-gateway",
+    "auth-service",
+    "ticket-service",
+    "attachment-service",
+    "notification-service",
+    "ticketdesk-ui"
+  ])
+  name                 = each.value
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Environment = "Dev"
+  }
+}
